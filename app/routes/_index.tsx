d0eb5f399c0d7ab33bba38745b881f1b3c1a8fdf
guilhpupo <guilh.pupo@gmail.com>
2023-04-16 00:45:29 -0300

    challenge completed

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,17 @@
 import type { V2_MetaFunction } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "QR Code Component Main" }];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main className="flex min-h-screen  content-center items-center bg-slate-200 ">
+      <section className="flex flex-col items-center space-y-4 p-3 pb-8 mx-auto bg-white shadow-md rounded-2xl">
+        <img src="image-qr-code.png" alt="An QR Code" className="w-60 rounded-xl" />
+        <h1 className="max-w-[15rem] text-center font-bold text-lg leading-tight ">Improve your front-end skills by building projects</h1>
+        <p className="max-w-[15rem] text-center text-sm text-gray-400">Scan the QR Code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </section>
+    </main>
   );
 }
